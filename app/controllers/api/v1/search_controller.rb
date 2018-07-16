0241f3f179f0bb2f16e233e63f3d288d98d27b88
Luis Rodriguez <luisrodge@outlook.com>
2018-07-16 16:58:44 -0600

    Update search feature

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::SearchController < ApplicationController
     
     def search
         listings = Listing.search(params[:query])
-        render json: listings, adapter: :json
+        if listings.empty?
+            head :ok
+        else
+            render json: listings, adapter: :json
+        end
     end
 end
