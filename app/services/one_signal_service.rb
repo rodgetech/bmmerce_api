4a98414731e0f99d5fe8b828784942e3de964315
Luis Rodriguez <luisrodge@outlook.com>
2018-10-02 13:17:04 -0600

    Update onesignal service new listing notification

--- app/services/one_signal_service.rb
+++ app/services/one_signal_service.rb
@@ -38,16 +38,21 @@ class OneSignalService
   end
 
   def new_listing_notification
-    player_ids = Account.where.not(id: @record.account.id).near([@record.latitude, @record.longitude], 25).each.pluck(:player_id)
-    notification_body = { 
-      "app_id" => APP_ID,
-      "include_player_ids" => player_ids,
-      "contents" => { "en" => "#{@record.title}" },
-      "headings" => {"en" => "New item posted near you"},
-      "big_picture" => @record.images.first.listing_image.url,
-      "data" => {"listingId": "#{@record.id}"}
-    }.to_json
-    send_notification(notification_body)
+    # player_ids = Account.where.not(id: @record.account.id).near([@record.latitude, @record.longitude], 25).each.pluck(:player_id)
+    Account.where.not(id: @record.account.id).near([@record.latitude, @record.longitude], 25).find_each do |account|
+      notification_body = { 
+        "app_id" => APP_ID,
+        # "include_player_ids" => player_ids,
+        "contents" => { "en" => "#{@record.title}" },
+        "headings" => {"en" => "New item posted near you"},
+        "big_picture" => @record.images.first.listing_image.url,
+        "data" => {"listingId": "#{@record.id}"},
+        "filters" => [
+          {"field": "tag", "key": "userId", "relation": "=", "value": "#{account.id}"}, 
+        ]
+      }.to_json
+      send_notification(notification_body)
+    end
   end
 
 end
