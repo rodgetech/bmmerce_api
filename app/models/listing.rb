13ccd13ab60664108a1f372fd8066b3043f11c54
Luis Rodriguez <luisrodge@outlook.com>
2018-10-19 18:24:35 -0600

    Add address validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,6 +9,6 @@ class Listing < ApplicationRecord
     has_many :images, dependent: :destroy
     has_many :engagements, dependent: :destroy
 
-    validates_presence_of :title
+    validates_presence_of :title, :address
     validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
