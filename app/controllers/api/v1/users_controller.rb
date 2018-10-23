598344ee255a0de5385c48d283b6a869452c4c9a
Luis Rodriguez <luisrodge@outlook.com>
2018-10-23 17:23:41 -0600

    Add created at to user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
     before_action :set_users
+    before_action :set_user, only: :show
 
     def index
         render json: @users, 
@@ -7,10 +8,18 @@ class Api::V1::UsersController < ApplicationController
             adapter: :json
     end
 
+    def show
+        render json: @user, adapter: :json
+    end
+
     private
     
     def set_users
         @users = User.order(created_at: :desc).page(params[:page]).per(params[:per])
         @total_pages = User.page(1).per(params[:per]).total_pages
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
--- app/serializers/api/v1/admin/user_serializer.rb
+++ app/serializers/api/v1/admin/user_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::Admin::UserSerializer < ActiveModel::Serializer
-    attributes :id, :name, :email, :avatar, :password_digest, :address
+    attributes :id, :name, :email, :avatar, :address, :created_at
 
     def password_digest
         ""
